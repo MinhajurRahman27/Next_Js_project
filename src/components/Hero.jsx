1f45a3dde4c009744cf5c9d0fbc843632d74faa7
Minhajur rahman <minhajurrahman2716@gmail.com>
2026-01-15 21:08:45 +0600

    Hero section finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,20 @@ import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
+
+  
   return (
-    <div className="flex justify-center pt-54 ">
-      <div className="relative">
+    <div className="flex relative justify-around gap-10 pt-54 ">
+     
+      <div className=" w-[600px]">
+        <div className="  text-7xl  text-indigo-600 ">
+      AK 47
+      </div>
+     <span className="text-gray-500 font-light">
+      <span className="text-3xl font-semibold ">T</span>he AK-47, designed by Mikhail Kalashnikov in 1947, became one of the most widely recognized firearms in history. Its designation stands for 'Avtomat Kalashnikova 1947'. The rifle was designed for reliability in harsh conditions, featuring a simple gas-operated mechanism with a rotating bolt. Its robust construction allows it to function in extreme temperatures, mud, sand, and with minimal maintenance. 
+     </span>
+      </div>
+      <div className="relative border-amber-200">
         
         {/* Image Card */}
         <div className=" rounded-2xl  relative z-10 ">
@@ -34,6 +45,7 @@ const Hero = () => {
         </div>
 
       </div>
+ 
     </div>
   );
 };
